a842876a81e54e82d57f2c289cc5911ef8fb2e56
Clive Lai <93177532+clivelai@users.noreply.github.com>
2022-01-28 21:03:56 +0800

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -162,8 +162,6 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media'),
-    os.path.join(BASE_DIR, 'static_cdn'),
-    os.path.join(BASE_DIR, 'media_cdn'),
 ]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
