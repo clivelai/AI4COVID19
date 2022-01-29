eed834c8ae28133b177893f0967c1a06af6f701d
Clive Lai <93177532+clivelai@users.noreply.github.com>
2022-01-30 01:39:19 +0800

    some changes

--- static/js/diagnose/diagnose.js
+++ static/js/diagnose/diagnose.js
@@ -188,14 +188,18 @@ window.onload = function Init() {
     }
     navigator.getUserMedia({audio: true}, startUserMedia, function(e) {alert('No live audio input: ' + e);});
 
+    
+
     if (btnMicrophone.addEventListener) {  // all browsers except IE before version 9
-        btnMicrophone.addEventListener ("pointerdown", function (e) {
+        btnMicrophone.addEventListener ("mousedown", function (e) {
             e.preventDefault(); 
+            e.stopPropagation();
             OnButtonDown (btnMicrophoneBorder);
             startRecording();
         }, false);
-        btnMicrophone.addEventListener ("pointerup", function (e) {
+        btnMicrophone.addEventListener ("mouseup", function (e) {
             e.preventDefault(); 
+            e.stopPropagation();
             OnButtonUp (btnMicrophoneBorder);
             if (recordingCount < 6) {
                 pauseRecording();
